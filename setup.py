90fc429042ef9dc9ebe4d6f12122211073af3d0c
Danijel-James Wynyard <danijel.james@me.com>
2018-02-19 07:11:14 +1100

    init setup.py v0.0.1

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='0.0.1',
+      description='Python OpenWeather Twitter Bot',
+      author='Danijel-James Wynyard',
+      author_email='danijel.james@gmail.com',
+      url='https://github.com/danijeljw/PyOWTB/',
+      packages=['distutils', 'distutils.command'],
+     )
