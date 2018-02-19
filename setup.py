f335afaafacc7b808f39635e9335299f2f36e2dc
Danijel-James Wynyard <danijel.james@me.com>
2018-02-19 21:43:51 +1100

    v0.0.2 versioning update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='Distutils',
-      version='0.0.1',
+      version='0.0.2',
       description='Python OpenWeather Twitter Bot',
       author='Danijel-James Wynyard',
       author_email='danijel.james@gmail.com',
