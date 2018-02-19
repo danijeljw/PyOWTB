7d05994dbf65cfdf54949a5954a21e5688e3dd9c
Danijel-James Wynyard <danijel.james@me.com>
2018-02-19 18:58:23 +1100

    updated infile notes

--- keys.py
+++ keys.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
-#keys.py
-#visit http://dev.twitter.com to create an application and get your keys
+
+# keys.py
+
+# Keys required for connection to Twitter account
+# Can be obtained from https://dev.twitter.com
+
+
+
 keys = dict(
-	consumer_key =          'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-	consumer_secret =       'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-	access_token =          'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-	access_token_secret =   'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    consumer_key =          'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    consumer_secret =       'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    access_token =          'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    access_token_secret =   'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
 )
